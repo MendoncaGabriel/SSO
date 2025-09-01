30b940e0eea23f41be016e26d83503728f2c79d3
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-09-01 10:19:02 -0400

    feat: create endpoint get client by id

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
 
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
 
-  @Get("client")
-  async list(){
+  @Get("")
+  async listClients(){
     return await this.clientService.list();
+  }
+
+  @Get(":id")
+  async findById(@Param() id: string){
+    return await this.clientService.getById({id});
   }
 
 
