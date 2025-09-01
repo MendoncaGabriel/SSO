cd3c3e688bc3978e0a4d73f8660f5487ea936fe6
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-09-01 10:33:36 -0400

    feat: adiciondo endpoint crud para client

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,19 +1,40 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ClientService } from './client.service';
+import { CreateClientDTO } from './dto/create.client';
+import { UpdateClientDTO } from './dto/update.client';
 
 @Controller('client')
 export class ClientController {
   constructor(private readonly clientService: ClientService) {}
+  @Post()
+  async create(@Body() body:CreateClientDTO){
+    return await this.clientService.create(body);
+  }
 
   @Get("")
-  async listClients(){
+  async list(){
     return await this.clientService.list();
   }
 
   @Get(":id")
-  async findById(@Param() id: string){
+  async getById(@Param() id: string){
     return await this.clientService.getById({id});
   }
 
+  @Patch(":id")
+  async update(
+    @Param() {id}, 
+    @Body() body: UpdateClientDTO
+  ){
+    return await this.clientService.update({
+      id,
+      name: body.name,
+      url: body.url
+    })
+  }
 
+  @Delete()
+  async delete(@Param() {id}){
+    return await this.clientService.delete(id);
+  }
 }
