8abc79a6c13b68bd9c46bde1a23b6556ede97903
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-09-02 06:13:34 -0400

    feat: adicionado crate role controller

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { ConflictException, Controller, Post } from '@nestjs/common';
 import { RoleService } from './role.service';
+import { CreateRoleDTO } from './dto/create.role.dto';
 
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
+  
+  @Post()
+  async create(data: CreateRoleDTO){
+    const {roles} = await this.roleService.listByPermissionId(data.permissionId);
+    
+    const existingRules = roles.filter(e => {
+      e.action === data.action &&
+      e.permissionId === data.action && 
+      e.resource === data.action
+    })
+
+    if(existingRules.length !== 0){
+      throw new ConflictException("This role already exists")
+    }
+
+    return await this.roleService.create(data);
+  }
 }
